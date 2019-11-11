3fe1e4019dcef2e18259ea25dc5fdb8dfb2639d2
callmewaggs <zxv74014@gmail.com>
2019-11-11 12:15:53 +0900

    초기 환경 구축  - 의존성 설정  - WebRTC  - Database 연결

---
+++ src/main/java/com/github/callmewaggs/youngwildfreelive/YoungWildFreeLiveApplication.java
@@ -0,0 +1,13 @@
+package com.github.callmewaggs.youngwildfreelive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoungWildFreeLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoungWildFreeLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/callmewaggs/youngwildfreelive/YoungWildFreeLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.callmewaggs.youngwildfreelive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoungWildFreeLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
